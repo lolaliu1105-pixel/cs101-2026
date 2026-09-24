d5961e1084bb791c350ba1326ea569ab9226df9c
lolaliu1105-pixel <lolaliu1105@gmail.com>
2026-09-24 13:37:27 +0800

    Add calendar display for September 2026
    Implement a calendar display for September 2026.

---
+++ ex1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\t\t2026 9月\n");
+    printf("日\t一\t二\t三\t四\t五\t六\n");
+    printf("\033[1;34m30\t31\t");
+    printf("\033[1;36m1\t2\t3\t4\t5\n");
+    printf("6\t7\t8\t9\t10\t11\t12\n");
+    printf("13\t14\t15\t16\t17\t18\t19\n");
+    printf("20\t21\t22\t23\t24\t25\t26\n");
+    printf("27\t28\t29\t30\t");
+    printf("\033[1;34m1\t2\t3\n");
+    
+    return 0;
+}
